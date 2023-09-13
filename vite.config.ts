1266a82d599339f2b0c7c5ad90cc6bc0c1d6ee54
bebossi <bernardobossi77@gmail.com>
2023-09-13 05:58:39 -0300

    npm update

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
-  },
+  // define: {
+  //   "process.env": {},
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/Components/ui"),
