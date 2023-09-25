4e810f702973be33bbf79e3c950ec9e324f9c26b
bebossi <bernardobossi77@gmail.com>
2023-09-25 10:47:52 -0300

    delete unused file

--- src/auth/socialLogin.ts
+++
@@ -1,7 +0,0 @@
-import { apiURLs } from "../api";
-
-export function GoogleLogin() {
-  const googleAuth = () => {
-    window.open(`${apiURLs["development"]}/auth/google/callback`, "_self");
-  };
-}
