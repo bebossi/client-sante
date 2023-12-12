b2b686294cca3f52722eb171efd5e57638a1883f
bebossi <bernardobossi77@gmail.com>
2023-12-12 15:26:15 -0300

    add cypress folder to src

---
+++ src/cypress/support/commands.ts
@@ -0,0 +1,76 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+// import "cypress-file-upload";
+
+export function login() {
+  cy.intercept("POST", "/login", {
+    statusCode: 200,
+    headers: {
+      "set-cookie":
+        "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJlOGRhMWFhLTFmMmUtNGQ1Yy1hYTQzLTM2MjBkNmEyMGEyZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5NTAzOTY1MSwiZXhwIjoxNjk1MTI2MDUxfQ.Lnv8yiA1bFQUI1vHf5YUNdDzGOMwEdcN7HKTRtRJvyY; HttpOnly secure; path=/; samesite=none;",
+    },
+  });
+
+  cy.visit("/", { timeout: 3000 });
+  cy.get('[data-cy="userMenu"]', { timeout: 3000 }).click();
+  cy.get('[data-cy="Entre"]', { timeout: 3000 }).click();
+  cy.intercept("GET", "/currentUser", {
+    statusCode: 200,
+    body: {
+      role: "admin",
+    },
+  }).as("currentUser");
+  cy.get("input#email", { timeout: 3000 }).click().type("aaaaaaa@teste");
+  cy.get("input#password", { timeout: 3000 }).click().type("teste");
+  cy.get('[data-cy="Continue"]', { timeout: 3000 }).click();
+
+  // cy.contains("Usuario logado").should("exist");
+}
+
+Cypress.on("uncaught:exception", (err) => {
+  if (
+    err.message.includes("Request failed with status code 403") ||
+    err.message.includes("Request failed with status code 401") ||
+    err.message.includes(
+      "TypeError: Cannot read properties of null (reading 'document')"
+    )
+  ) {
+    return false;
+  }
+  //   return true;
+});
